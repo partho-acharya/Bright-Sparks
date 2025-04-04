fc5e34f796b193211141be239adcdf042f7c0d90
parthoAcharya <acharyapartho411@gmail.com git config --global user.email acharyapartho411@gmail.com>
2025-04-04 10:40:06 +0530

    whatsapp button add

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function hideMenu() {
 }
 
 
-
+// nav scroll color start
 // header fixed jquery
 
 $(window).on("scroll", function () {
@@ -20,4 +20,33 @@ $(window).on("scroll", function () {
         $('nav').removeClass('black');
     }
 });
+// nav scroll color end
+
+// whatsapp floot Btn start
+// Add click animation
+const whatsappBtn = document.querySelector('.whatsapp-btn');
+
+whatsappBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Add click animation
+    this.style.transform = 'scale(0.9)';
+    setTimeout(() => {
+        this.style.transform = 'scale(1)';
+    }, 200);
+
+    // Open WhatsApp after animation
+    setTimeout(() => {
+        window.open(this.href, '_blank');
+    }, 300);
+});
+
+// Optional: Hide button when printing
+window.addEventListener('beforeprint', () => {
+    whatsappBtn.style.display = 'none';
+});
+window.addEventListener('afterprint', () => {
+    whatsappBtn.style.display = 'flex';
+});
+// whatsapp floot Btn end
 
